e18d0497bfd52785fd07fae091a6771385bfb88e
Artem Kudria <ak98nneon@gmail.com>
2018-11-19 14:44:00 +0200

    ADDING COMMENTS IDEP, IEMPL

--- src/main/java/com/ak98neon/dao/IDepartmentWorker.java
+++ src/main/java/com/ak98neon/dao/IDepartmentWorker.java
@@ -5,23 +5,57 @@ import com.ak98neon.model.Department;
 import java.util.List;
 
 public interface IDepartmentWorker {
+    /**
+     * Create table Department
+     *
+     * @return true or false
+     */
     boolean createTable();
 
+    /**
+     * Insert Department in data base
+     *
+     * @param name String name Name Department
+     * @return true or false
+     */
     boolean insertDepartment(final String name);
 
+    /**
+     * Update department
+     *
+     * @param id      Long id Department
+     * @param newName String new name is Department
+     * @return true or false
+     */
     boolean updateDepartment(final Long id, final String newName);
 
+    /**
+     * Delete department
+     *
+     * @param id Long id department
+     * @return true or false
+     */
     boolean deleteDepartment(final long id);
 
     /**
      * Select Department by id
      *
-     * @param id id department
+     * @param id Long id department
      * @return Department object
      */
     Department selectById(final long id);
 
+    /**
+     * Select All Departments
+     *
+     * @return List Departments
+     */
     List selectAllDepartments();
 
+    /**
+     * Drop table department
+     *
+     * @return true or false
+     */
     boolean dropTable();
 }
--- src/main/java/com/ak98neon/dao/IEmployeeWorker.java
+++ src/main/java/com/ak98neon/dao/IEmployeeWorker.java
@@ -5,20 +5,72 @@ import com.ak98neon.model.Employee;
 import java.util.List;
 
 public interface IEmployeeWorker {
+    /**
+     * Create table Employee
+     *
+     * @return true or false
+     */
     boolean createTable();
 
+    /**
+     * Insert Employee
+     *
+     * @param firstName    String First name employee
+     * @param lastName     String Last name employee
+     * @param age          int age emplpyee
+     * @param mail         String mail employee
+     * @param departmentId Long ID Department
+     * @return true or false
+     */
     boolean insertEmployee(final String firstName, final String lastName, final int age, final String mail,
                            final long departmentId);
 
+    /**
+     * Update Employee
+     *
+     * @param firstName String First name employee
+     * @param lastName  String Last name employee
+     * @param age       int age emplpyee
+     * @param mail      String mail employee
+     * @return true or false
+     */
     boolean updateEmployee(final Long id, final String firstName, final String lastName, final int age, final String mail);
 
+    /**
+     * Delete Employee
+     *
+     * @param id Long id employee
+     * @return true or false
+     */
     boolean deleteEmployee(final long id);
 
+    /**
+     * Select by id employee
+     *
+     * @param id Long id employee
+     * @return Employee
+     */
     Employee selectByIdEmployee(final long id);
 
+    /**
+     * Select all employees
+     *
+     * @return List Employee
+     */
     List<Employee> selectAllEmployees();
 
+    /**
+     * Select all employee from by department to id Department
+     *
+     * @param idDepartment Long id department
+     * @return List Employee
+     */
     List<Employee> selectAllEmployeesByDepartment(final long idDepartment);
 
+    /**
+     * Drop table
+     *
+     * @return true or false
+     */
     boolean dropTable();
 }
